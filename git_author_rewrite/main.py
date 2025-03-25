60483afeb197fe9f6e60f88f7b3ab504edfeaf00
ideftbuild <a.akanSWE@gmail.com>
2025-03-25 19:31:24 +0100

    feat(main): add reset option and refactor code for reusability
       - Implemented the `--reset` option to allow branch reset.
       - Refactored related logic into functions for better reusability.
       - Improved documentation for clarity.

--- git_author_rewrite/main.py
+++ git_author_rewrite/main.py
@@ -39,7 +39,7 @@ def is_email_format_valid(email: str) -> bool:
     return False if re.match(pattern, email) is None else True
 
 
-def create_parser():
+def create_parser() -> argparse.ArgumentParser:
     """Create and return a command-line argument parser.
 
     Returns:
@@ -54,9 +54,48 @@ def create_parser():
     parser.add_argument('-n', '--name', nargs=2, metavar=('old_name', 'new_name'), help='Provide the old name to update and the new name')
     parser.add_argument('-e', '--email', nargs=2, metavar=('old_email', 'new_email'), help='Provide the old email to update and the new email')
     parser.add_argument('-l', '--local-path', help='Provide the path to the local repository')
+    parser.add_argument('-r', '--reset', help='Reset for a fresh start')
     
-
     return parser
+
+
+def create_working_branch(repo: git.Repo) -> git.HEAD:
+    """Create working branch for which the changes will be applied too
+
+    Args:
+        repo (git.Repo): the repo
+
+    Returns:
+        git.HEAD: the working branch
+    """
+
+    rewrite_branch = repo.create_head(BRANCH_NAME)
+    display_messages(f'Created new branch: ' + BRANCH_NAME)
+
+    rewrite_branch.checkout()
+    display_messages(f'Switched to new branch: {BRANCH_NAME}')
+    return rewrite_branch
+
+
+def sync_working_branch(repo: git.Repo, default_branch: git.HEAD) -> git.HEAD:
+    """Sync the working branch with the default branch.
+
+    Args:
+        repo (git.Repo): The Git repository.  
+        default_branch (git.HEAD): The default branch to sync with.  
+
+    Returns:
+        git.HEAD: The updated working branch.  
+    """
+
+    if repo.active_branch.name != BRANCH_NAME:
+        rewrite_branch = repo.heads[BRANCH_NAME]
+        rewrite_branch.checkout()
+        display_messages(f"Switched to '{BRANCH_NAME}'")
+
+    repo.git.merge(default_branch)  # Update the branch 
+    display_messages(f"Merged '{default_branch.name}' to '{BRANCH_NAME}'")
+    return rewrite_branch
 
 
 def main(args: list = sys.argv[1:]):
@@ -65,17 +104,18 @@ def main(args: list = sys.argv[1:]):
     Args:
         args (list, optional): command-line arguments. defaults to sys.argv[1:].
     """
-
     parser = create_parser()
 
     args = parser.parse_args()
 
     repo = git.Repo(args.local_path if args.local_path else '.')  # Connect to repo
 
-    if 'origin' not in repo.remotes:
-        display_messages("No remote named 'origin' found.", status=Status.ERROR)
+    print('repo:', repo)
 
-    default_branch = repo.heads.main if 'main' in repo.heads else repo.heads.master  # Get default branch
+    if 'origin' not in repo.remotes:
+        display_messages("No remote named 'origin' found", status=Status.ERROR)
+    
+    default_branch = repo.heads.main if 'main' in repo.heads else repo.heads.master;
 
     repo.git.checkout(default_branch.name)
     display_messages(f"Switched to '{default_branch.name}'")
@@ -83,36 +123,23 @@ def main(args: list = sys.argv[1:]):
     repo.remotes.origin.pull(refspec=default_branch.name)
     display_messages('Pulled latest changes from remote')
 
+    # Delete working branch if --reset is passed  
+    if args.reset:
+        repo.git.branch('-D', BRANCH_NAME)
+
+    rewrite_branch = None
     try:
         if BRANCH_NAME in repo.heads:
-            if repo.active_branch.name != BRANCH_NAME:
-                repo.git.checkout(BRANCH_NAME)  # Switch to branch if it exists              
-                display_messages(f"Switched to '{BRANCH_NAME}'")
-
-            repo.git.merge(default_branch)  # Update the branch 
-            display_messages(f"Merged '{default_branch}' to '{BRANCH_NAME}'")
+            rewrite_branch = sync_working_branch(repo, default_branch)
         else:
-            rewrite_branch = repo.create_head(BRANCH_NAME)
-            display_messages(f'Created new branch: ' + BRANCH_NAME)
-
-            rewrite_branch.checkout()
-            display_messages(f'Switched to new branch: {BRANCH_NAME}')
+            # Create branch if --reset is passed or it doesn't exist  
+            rewrite_branch = create_working_branch(repo)
     except Exception as e:
         display_messages(f"Error: Failed to rewrite branch '{BRANCH_NAME}': {e}",
                          'To proceed, you can:',
                          '  - Fix the errors and retry.',
                          "  - Use the '-d' or '--delete' option to delete this branch and start fresh.",
                          status=Status.ERROR)
-        # repo.git.branch('-D', BRANCH_NAME)
-        # print(f'Deleted {BRANCH_NAME} branch')
-        # print(f'Cleaning up...')
-
-
-
-
-    # print('Done rewritting...')
-    # repo.git.checkout('main')
-    # print('Switched to 'main'')
 
 
 if __name__ == '__main__':
