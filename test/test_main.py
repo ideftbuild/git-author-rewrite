b455c6822d233a90dac75410786b56b6cbdc60a4
ideftbuild <a.akanSWE@gmail.com>
2025-03-25 19:26:37 +0100

    test(main): add test cases

--- test/test_main.py
+++ test/test_main.py
@@ -1,8 +1,69 @@
 #!/usr/bin/env python3
 
 import pytest
-from git_author_rewrite.main import display_messages, main, is_email_format_valid
+from git_author_rewrite.main import (
+    git,
+    display_messages,
+    main,
+    is_email_format_valid,
+    create_working_branch,
+    sync_working_branch,
+)
+from unittest.mock import MagicMock
 from git_author_rewrite.enums import Status
+
+TEST_BRANCH_NAME = 'author-rewrite'
+
+@pytest.fixture
+def mock_repo():
+    """Mock the git.Repo object to avoid real repo modifications."""
+    repo = MagicMock(spec=git.Repo)
+    rewrite_branch = MagicMock()
+    default_branch = MagicMock()
+
+    repo.create_head.return_value = rewrite_branch
+    repo.git.merge.return_value = None
+    repo.active_branch.name = 'main'
+    repo.heads = { 'main': default_branch, 'author-rewrite': rewrite_branch }
+
+    return repo
+
+
+def test_create_working_branch(mock_repo):
+    """Test that the function correctly creates and checks out a branch."""
+    branch = create_working_branch(mock_repo)
+
+    # Check if create_head was called with the correct branch name
+    mock_repo.create_head.assert_called_once_with(TEST_BRANCH_NAME)
+
+    # Check if checkout() was called once
+    branch.checkout.assert_called_once()
+
+
+def test_sync_working_branch_not_on_active_branch(mock_repo):
+    """Test syncing the working branch with the default branch."""  
+    default_branch = mock_repo.heads['main']
+
+    rewrite_branch = sync_working_branch(mock_repo, default_branch)
+
+    # Check if merge was called with the correct branch name
+    mock_repo.git.merge.assert_called_once_with(default_branch)
+
+    # Check if checkout() was called once 
+    rewrite_branch.checkout.assert_called_once()
+
+def test_sync_working_branch_on_active_branch(mock_repo):
+    """Test syncing the working branch with the default branch."""  
+    default_branch = mock_repo.heads['main']
+    mock_repo.active_branch.name = 'main'
+
+    rewrite_branch = sync_working_branch(mock_repo, default_branch)
+
+    # Check if merge was called with the correct branch name
+    mock_repo.git.merge.assert_called_once_with(default_branch)
+
+    # Check if checkout() was called once 
+    rewrite_branch.checkout.assert_called_once()
 
 
 def test_display_messages_exits_with_error_and_prints_to_stderr(capfd):
